5cb8d72b392d96ed4cf39fd0e43922f3af9ad550
Chao Ma <machaosydney@gmail.com>
2023-09-18 12:17:56 +1000

    feat: implement modal provider and hooks component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,30 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+
 import { ClerkProvider } from '@clerk/nextjs';
+
+import { ModalProvider } from '@/providers/modal-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-	title: 'Admin Dashboard',
-	description: 'Admin Dashboard',
+  title: 'Admin Dashboard',
+  description: 'Admin Dashboard',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<ClerkProvider>
-			<html lang='en'>
-				<body className={inter.className}>{children}</body>
-			</html>
-		</ClerkProvider>
-	);
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
